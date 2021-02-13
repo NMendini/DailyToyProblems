a9c5a1da481eebd1230c04ef1df1651758e8a568
Nicholas Mendini <nicholas.mendini@gmail.com>
2021-02-13 15:34:11 -0600

    Implemented majorityElement

--- majorityElement/majorityElement.js
+++ majorityElement/majorityElement.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Given an array nums of size n, return the majority element.
 
@@ -27,8 +28,31 @@ Follow-up: Could you solve the problem in linear time and in O(1) space?
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+const majorityElement = (nums) => {
+  // create counts object
+  const counts = {};
+  // create len variable equal to length of nums
+  const len = nums.length;
 
+  // iterate over nums
+  for (let i = 0; i < len; i += 1) {
+    const current = nums[i];
+    // if counts at current is undefined
+    if (counts[current] === undefined) {
+      // set counts at current to 1
+      counts[current] = 1;
+      // otherwise
+    } else {
+      // increment counts at current
+      counts[current] += 1;
+    }
+
+    // if counts at current is greater than len
+    if (counts[current] > len / 2) {
+      // return current
+      return current;
+    }
+  }
 };
 
 // ASSERTION FUNCTION
@@ -41,3 +65,10 @@ const assertEqual = (actual, expected, testName) => {
 };
 
 // TEST SUITE
+const actual1 = majorityElement([3, 2, 3]);
+const expected1 = 3;
+assertEqual(actual1, expected1, 'it should return the element that appears most often');
+
+const actual2 = majorityElement([2, 2, 1, 1, 1, 2, 2]);
+const expected2 = 2;
+assertEqual(actual2, expected2, 'it should return the element that appears most often');
